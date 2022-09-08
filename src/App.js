34294048bf74143b8d41e5cc93b4fe08efc93acc
John Oblenda <joandong2@gmail.com>
2022-09-07 23:17:32 -0400

    working on template

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import PokemonList from './features/PokemonList';
 function App() {
   return (
     <div className="App">
+      <section id="Hero">
+        <div className='container mx-auto flex items-center md:flex-column'>
+          <img src='./assets/497-4973553_charizard-pokemon-charizard-png-clipart.png' alt="" />
+          <h1 className="text-5xl font-bold bg-red-800 ml-8">Pokemon</h1>
+        </div>
+      </section>
       <h1 className="font-bold bg-red-800">Pokemon</h1>
       <PokemonList />
     </div>
